46d3c36adbd8bcd34f50985ec12d9cd09f089b8e
ernest <ernestsagibalov@gmail.com>
2021-11-07 18:39:14 +0200

    user can get more info about cake

--- src/main/java/com/eshagibalov/cakesShop/goods/CakesService.java
+++ src/main/java/com/eshagibalov/cakesShop/goods/CakesService.java
@@ -1,7 +1,11 @@
 package com.eshagibalov.cakesShop.goods;
 
+import com.eshagibalov.cakesShop.rest.dto.CakeMoreInfo;
 import com.eshagibalov.cakesShop.rest.dto.Cakes;
 
 public interface CakesService {
+
     Cakes getCakes();
+
+    CakeMoreInfo getMoreInfo(Long id);
 }
